839b9425249616f5c1ab9691c1d877a87a82ccae
Christopher Stott <christopherstott@gmail.com>
2015-11-21 16:38:37 -0800

    Framework stub

---
+++ RaptureXMLFrameworkStub/RaptureXMLFrameworkStub.h
@@ -0,0 +1,19 @@
+//
+//  RaptureXMLFrameworkStub.h
+//  RaptureXMLFrameworkStub
+//
+//  Created by Chris on 2015-11-21.
+//  Copyright © 2015 Rapture In Venice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RaptureXMLFrameworkStub.
+FOUNDATION_EXPORT double RaptureXMLFrameworkStubVersionNumber;
+
+//! Project version string for RaptureXMLFrameworkStub.
+FOUNDATION_EXPORT const unsigned char RaptureXMLFrameworkStubVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RaptureXMLFrameworkStub/PublicHeader.h>
+
+
